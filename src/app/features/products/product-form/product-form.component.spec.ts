4c7613a802a0185a5c903622fe80bd16b3acaec7
Salvador Toj <salvador.toj.dev@gmail.com>
2026-04-27 19:57:39 -0600

    test(products): add NzMessageModule to ProductFormComponent spec

--- src/app/features/products/product-form/product-form.component.spec.ts
+++ src/app/features/products/product-form/product-form.component.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
+import { importProvidersFrom } from '@angular/core';
 import { ProductFormComponent } from './product-form.component';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { NZ_I18N, es_ES } from 'ng-zorro-antd/i18n';
+import { NzMessageModule } from 'ng-zorro-antd/message';
 
 describe('ProductFormComponent', () => {
   beforeEach(async () => {
@@ -14,6 +16,7 @@ describe('ProductFormComponent', () => {
         provideHttpClient(),
         provideAnimationsAsync(),
         { provide: NZ_I18N, useValue: es_ES },
+        importProvidersFrom(NzMessageModule),
       ],
     }).compileComponents();
   });
